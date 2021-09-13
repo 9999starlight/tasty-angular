79af8d7983bb86ba64d141b15d5c2f0da479f1c3
9999starlight <sestakirena@gmail.com>
2021-09-13 11:43:01 +0200

    rating style completed

--- client/src/app/modules/recipe-details/rating/rating.component.ts
+++ client/src/app/modules/recipe-details/rating/rating.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-rating',
@@ -6,18 +8,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rating.component.scss']
 })
 export class RatingComponent implements OnInit {
+  // login status
+  isLogged$: BehaviorSubject<boolean | null>;
+  // props & Output
+  @Input() recipeId: string = '';
+  @Output() updateMsgStatus = false;
   ratingDropdown: boolean = false;
   rateValue: number = 1;
-  constructor() { }
+
+  constructor(private el: ElementRef, private authService: AuthService) {
+    this.isLogged$ = this.authService.isLogged$.getValue();
+   }
 
   ngOnInit(): void {
+    //console.log('from rating: ', this.isLogged$)
   }
 
   toggleRating(){
     this.ratingDropdown = !this.ratingDropdown
   }
 
-  fillTheStars() {}
+  fillTheStars(value: any) {
+    //console.log(value.target.value)
+    let stars = this.el.nativeElement.querySelectorAll('.sp');
+    stars.forEach((st: any) => {
+      if(st.id <= Number(value.target.id)) {
+        st.classList.add('coloring')
+      } else {
+        st.classList.remove('coloring')
+      }
+    })
+  }
 
-  rateThisRecipe() {}
+  /* fillTheStars(event) {
+      this.rateValue = Number(event.target.id)
+      const stars = this.$refs.stars.children
+      stars.forEach((child) => {
+        if (Number(child.children[1].id) <= Number(event.target.id)) {
+          child.children[1].classList.add('coloring')
+        } else child.children[1].classList.remove('coloring')
+      })
+    } */
+
+  rateThisRecipe(value: any) {
+    this.rateValue = Number(value.target.id)
+    console.log(this.rateValue)
+    if(!this.isLogged$) {
+      
+      this.ratingDropdown = false;
+      this.rateValue = 1;
+      return;
+    }
+
+
+  }
 }
