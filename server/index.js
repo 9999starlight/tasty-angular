4e92e77cd4c54d98c1ca66f384bc5675e40f4ce3
irenasestak <sestakirena@gmail.com>
2025-03-11 12:54:49 +0100

    server update deps && node 20.18

--- server/index.js
+++ server/index.js
@@ -10,10 +10,10 @@ const mongoUrl = process.env.MONGO_URL
 //mongoose.set('useFindAndModify', false)
 
 mongoose
-  .connect(mongoUrl, {
+  .connect(mongoUrl/*, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  })
+  }*/)
   .then(() => console.log('MongoDB connected!'))
   .catch((err) => {
     console.log(`MongoDB connection Error: ${err.message}`)
